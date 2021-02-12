c2449feaf651588cb0eb0820062a97eab386ae58
vilja langseth <viljall@stud.ntnu.no>
2021-02-12 12:57:31 +0100

    La til dockerDownload som laster ned bookface og laster ned og kjører docker installasjon

---
+++ dockerDownload.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+#Last ned bookface kode
+git clone https://git.cs.oslomet.no/kyrre.begnum/bookface
+
+#Laster ned docker programmet og kjører nedlastningen
+curl -fsSL https://get.docker.com -o get-docker.sh
+sudo sh get-docker.sh
+
