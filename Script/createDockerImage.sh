1fbda4a0d679b15db8dc827ce7818d1b19710bb6
nicholbs <nicholasbs@hotmail.com>
2021-02-17 12:20:50 +0000

    oppdatert script

--- Script/createDockerImage.sh
+++ Script/createDockerImage.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -x
 #x opsjon gjør at det blir utskrift av alle kommandoer
-docker build -t docker_image:v0 .
+
+#bli superuser
+
+#gå til bookface repository
+cd /home/ubuntu/bookface/
+
+#bygg ett image ut i fra bookface repository
+sudo docker build -t docker_image:v0 .
--- Script/createDockerInstance.sh
+++ Script/createDockerInstance.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 #x opsjon gjør at det blir utskrift av alle kommandoer
-docker run -e BF_DB_HOST=192.168.130.10 -e BF_DB_PORT=26257 -e BF_DB_NAME=bf -e BF_DB_USER=bfuser -e BF_WEBHOST=10.212.140.205 -e BF_FRONTPAGE_LIMIT=500 -d -P docker_image:v0
+sudo docker run -e BF_DB_HOST=192.168.130.10 -e BF_DB_PORT=26257 -e BF_DB_NAME=bf -e BF_DB_USER=bfuser -e BF_WEBHOST=10.212.140.205 -e BF_FRONTPAGE_LIMIT=500 -d -P docker_image:v0
