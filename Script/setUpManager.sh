c4d9836556a2ad93480cf3e317233e02c36c98bf
nicholbs <nicholasbs@hotmail.com>
2021-02-19 16:06:30 +0100

    laget skript for å lage nye maskiner og et for å gjøre klart manager

---
+++ Script/setUpManager.sh
@@ -0,0 +1,13 @@
+#!/bin/bash -x
+#x opsjon gjør at det blir utskrift av alle kommandoer
+
+#Gå til ubuntu directory
+cd /home/ubuntu/
+
+#erstatt default .bashrc fil på manager med .bashrc_manager filen fra robusteTjenester repository
+mv robusteTjenester/manager/.bashrc_manager /home/ubuntu/.bashrc
+#source nye .bashrc
+source .bashrc
+
+#Husk å endre på IP addresse i .bashrc til manager
+echo "Husk Aa endre pAa alle IP-addressene i .bashrc"
