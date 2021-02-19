5428de17f5deb0a1b8482192a32ca5dcfb41159e
Ubuntu <nicholasbs@hotmail.com>
2021-02-19 17:28:49 +0000

    laget ett fantastisk "lagNyMaskinViaTerminal" script

--- Script/dockerDownload.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#Last ned bookface kode
-git clone https://git.cs.oslomet.no/kyrre.begnum/bookface
-
-#Laster ned docker programmet og kjører nedlastningen
-curl -fsSL https://get.docker.com -o get-docker.sh
-sudo sh get-docker.sh
-
